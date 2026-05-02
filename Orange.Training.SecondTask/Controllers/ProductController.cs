3964bfb7a17c9f4f6b43c67ae13e509e8f2d8b51
Eyad Computer <eeyad610@gmail.com>
2026-05-02 17:26:39 +0300

    feat: secure order fetching and product catalog integration

---
+++ Orange.Training.SecondTask/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Data;
+
+namespace Orange.Training.SecondTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly NpgsqlConnection _connection;
+
+        public ProductController(NpgsqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = new List<object>();
+            try
+            {
+                if (_connection.State != ConnectionState.Open) await _connection.OpenAsync();
+
+                string query = "SELECT id, name, price FROM products ORDER BY name ASC";
+                using var cmd = new NpgsqlCommand(query, _connection);
+                using var reader = await cmd.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    products.Add(new
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Price = reader.GetDecimal(2)
+                    });
+                }
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
+            return Ok(products);
+        }
+    }
+}
